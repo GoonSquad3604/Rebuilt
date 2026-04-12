806e1453f82e1f743eb632a2c63de7228335a070
AndrewF14 <galacticapple14@gmail.com>
2026-04-12 13:52:36 -0400

    ig we pushing it all

--- 2026BBot/src/main/java/frc/robot/RobotContainer.java
+++ 2026BBot/src/main/java/frc/robot/RobotContainer.java
@@ -337,16 +337,16 @@ public class RobotContainer {
                 () -> superstructure.getCurrentSuperState() == CurrentSuperState.STOPPED));
 
     // cllimber testing
-    driverController.povUp().onTrue(Commands.runOnce(() -> climber.setPowerInnerRungs(-1)));
+    driverController.povUp().onTrue(Commands.runOnce(() -> climber.setPowerInnerRungs(-0.8)));
     driverController.povUp().onFalse(Commands.runOnce(() -> climber.setPowerInnerRungs(0.0)));
 
-    driverController.povDown().onTrue(Commands.runOnce(() -> climber.setPowerInnerRungs(1)));
+    driverController.povDown().onTrue(Commands.runOnce(() -> climber.setPowerInnerRungs(0.8)));
     driverController.povDown().onFalse(Commands.runOnce(() -> climber.setPowerInnerRungs(0.0)));
 
-    driverController.y().onTrue(Commands.runOnce(() -> climber.setPowerOuterRungs(1)));
+    driverController.y().onTrue(Commands.runOnce(() -> climber.setPowerOuterRungs(0.8)));
     driverController.y().onFalse(Commands.runOnce(() -> climber.setPowerOuterRungs(0.0)));
 
-    driverController.a().onTrue(Commands.runOnce(() -> climber.setPowerOuterRungs(-1)));
+    driverController.a().onTrue(Commands.runOnce(() -> climber.setPowerOuterRungs(-0.8)));
     driverController.a().onFalse(Commands.runOnce(() -> climber.setPowerOuterRungs(0.0)));
 
     /* operator */
